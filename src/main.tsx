d44484ca92fc0d1e2ea4a5eac3253c45919eb601
Gabriel Lucena <glucena@livedata.com>
2025-02-14 17:57:55 -0500

    Refactor HoursLostLateStarts component for improved data structure and chart configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
+import { HoursLostLateStarts } from "./components/charts/HoursLostLateStarts.tsx"
 import { LateStartCaseCounts } from "./components/charts/LateStartCaseCounts.tsx"
-// import FirstCaseDelayReasonsTable from "./components/charts/FirstCaseDelayReasons.tsx"
-// import ServiceWeekdayFirstCaseStatsTable from "./components/charts/ServiceWeekdayFirstCaseStatistics.tsx"
-// import FirstCaseSummaryStats from "./components/charts/FirstCaseSummaryStats.tsx"
-// import HoursLostLateStarts from "./components/charts/HoursLostLateStarts.tsx"
-// import LateStartCaseCounts from "./components/charts/LateStartCaseCounts.tsx"
 import { PercentFCOS } from "./components/charts/PercentFCOS.tsx"
 
 import "./index.css"
@@ -17,8 +13,7 @@ createRoot(document.getElementById("root")!).render(
       <div className="flex flex-row gap-6">
         <PercentFCOS />
         <LateStartCaseCounts />
-        {/* <LateStartCaseCounts />
-      <HoursLostLateStarts /> */}
+        <HoursLostLateStarts />
       </div>
       {/* <div className="flex flex-row gap-6">
         <ServiceWeekdayFirstCaseStatsTable />
