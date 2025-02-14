b796fa26f3e3253007c69f9d2f96c30ea9b90574
Gabriel Lucena <glucena@livedata.com>
2025-02-14 16:08:31 -0500

    Update package.json to include date-fns and refactor PercentFCOS component

--- src/components/ui/area-graph.tsx
+++ src/components/ui/area-graph.tsx
@@ -13,30 +13,33 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart"
 
-export interface ChartConfig {
-  [key: string]: {
-    color?: string
-    dataKey: string
-    formatter: (value: string) => string
-    label: string
-    tickFormatter?: (value: string) => string
-  }
+interface Axis<TData> {
+  label: string
+  dataKey: keyof TData
+  formatter: (value: unknown) => string
+  color?: string
+}
+
+export interface AreaGraphConfig<TData> {
+  x: Axis<TData>
+  y: Axis<TData>
 }
 
 interface AreaGraphProps<TData> {
   title: string
   subtitle: string
-  chartConfig: ChartConfig
+  configuration: AreaGraphConfig<TData>
   data: TData[]
 }
 
 export function AreaGraph<TData>({
   title,
   subtitle,
-  chartConfig,
+  configuration,
   data,
 }: AreaGraphProps<TData>) {
-  const [xAxisConfig, yAxisConfig] = Object.values(chartConfig)
+  const [xAxisConfig, yAxisConfig] = Object.values(configuration)
+
   // Calculate the minimum and maximum value of data range and round up to the nearest 10
   const maxYValue = 100
   // Math.ceil(Math.max(...data.map((d) => d[yAxisConfig.dataKey])) / 10) * 10
@@ -49,84 +52,77 @@ export function AreaGraph<TData>({
   const gradientId = `colorGradient-${yAxisConfig.color?.replace("#", "")}`
 
   return (
-    <Card className="visualization-container">
-      <CardHeader>
+    <Card className="flex flex-col">
+      <CardHeader className="flex flex-col">
         <CardTitle>{title}</CardTitle>
         <CardDescription>{subtitle}</CardDescription>
       </CardHeader>
-      <CardContent>
-        <div className="live-data-line-graph">
-          <ChartContainer config={chartConfig} className="h-[150px] w-[530px]">
-            <AreaChart accessibilityLayer data={data}>
-              <defs>
-                <linearGradient id={gradientId} x1="0" y1="0" x2="0" y2="1">
-                  <stop
-                    offset="0%"
-                    stopColor={
-                      yAxisConfig.color ? yAxisConfig.color : "#7B71FA"
-                    }
-                    stopOpacity={0.3}
-                  />
-                  <stop
-                    offset="100%"
-                    stopColor={
-                      yAxisConfig.color ? yAxisConfig.color : "#7B71FA"
-                    }
-                    stopOpacity={0.05}
-                  />
-                </linearGradient>
-              </defs>
-              <XAxis
-                dataKey={xAxisConfig.dataKey}
-                tickLine={false}
-                tickMargin={10}
-                axisLine={false}
-                tickFormatter={
-                  xAxisConfig.formatter
-                    ? xAxisConfig.formatter
-                    : (value) => value
-                }
-                // tick={<RotatedAxisTick />}
-              />
-              <ChartTooltip
-                content={
-                  <ChartTooltipContent
-                    hideIndicator={true}
-                    hideLabel={false}
-                    labelKey={yAxisConfig.dataKey}
-                  />
-                }
-                // formatter={
-                //   yAxisConfig.formatter
-                //     ? yAxisConfig.formatter
-                //     : (value) => value
-                // }
-              />
-              <YAxis
-                type="number"
-                tickLine={false}
-                axisLine={false}
-                tickFormatter={
-                  yAxisConfig.tickFormatter
-                    ? yAxisConfig.tickFormatter
-                    : (value) => value
-                }
-                domain={[minYValue, maxYValue]}
-                width={40} // Adjust the width of the Y-axis
-              />
-              <Area
-                dataKey={yAxisConfig.dataKey}
-                label={yAxisConfig.label}
-                type="natural"
-                stroke={yAxisConfig.color ? yAxisConfig.color : "#7B71FA"}
-                strokeWidth={3}
-                fill={`url(#${gradientId})`}
-                fillOpacity={1}
-                dot={false}
-              />
-            </AreaChart>
-          </ChartContainer>
-        </div>
+      <CardContent className="flex grow">
+        <ChartContainer
+          config={{
+            xAxisConfig: configuration.x,
+            yAxisConfig: configuration.y,
+          }}
+        >
+          <AreaChart accessibilityLayer data={data}>
+            <defs>
+              <linearGradient id={gradientId} x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="0%"
+                  stopColor={yAxisConfig.color ? yAxisConfig.color : "#7B71FA"}
+                  stopOpacity={0.3}
+                />
+                <stop
+                  offset="100%"
+                  stopColor={yAxisConfig.color ? yAxisConfig.color : "#7B71FA"}
+                  stopOpacity={0.05}
+                />
+              </linearGradient>
+            </defs>
+            <XAxis
+              dataKey={xAxisConfig.dataKey.toString()}
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={
+                xAxisConfig.formatter ? xAxisConfig.formatter : (value) => value
+              }
+              // tick={<RotatedAxisTick />}
+            />
+            <ChartTooltip
+              content={
+                <ChartTooltipContent
+                  hideIndicator={true}
+                  hideLabel={false}
+                  labelKey={yAxisConfig.dataKey.toString()}
+                />
+              }
+              formatter={
+                yAxisConfig.formatter ? yAxisConfig.formatter : (value) => value
+              }
+            />
+            <YAxis
+              type="number"
+              tickLine={false}
+              axisLine={false}
+              tickFormatter={
+                yAxisConfig.formatter ? yAxisConfig.formatter : (value) => value
+              }
+              domain={[minYValue, maxYValue]}
+              width={40}
+            />
+            <Area
+              dataKey={yAxisConfig.dataKey.toString()}
+              label={yAxisConfig.label}
+              type="natural"
+              stroke={yAxisConfig.color ? yAxisConfig.color : "#7B71FA"}
+              strokeWidth={3}
+              fill={`url(#${gradientId})`}
+              fillOpacity={1}
+              dot={false}
+            />
+          </AreaChart>
+        </ChartContainer>
       </CardContent>
     </Card>
   )
